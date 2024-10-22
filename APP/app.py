20f914e3faec72ca32d95dde2b5fcf229d36e23b
dice-2004 <135799601+dice-2004@users.noreply.github.com>
2024-10-22 15:42:34 +0900

    変更

--- APP/app.py
+++ APP/app.py
@@ -10,7 +10,7 @@ config_data = fetch.fetch()
 # notion = notion_class.Notion(config_data["notion_token"])
 
 # # print(config_data)
-# str = notion.get_database(config_data["database_id"])
+# str = notion.get_pages(config_data["database_id"])
 # print(str)
 
 intents =discord.Intents.default()
@@ -21,7 +21,7 @@ intents.message_content = True
 bot=commands.Bot(
         command_prefix="/",
         intents=intents,
-        activity=discord.Game("hogehoge"),
+        activity=discord.Game("いまうごいたわ"),
 )
 
 @bot.event
@@ -37,10 +37,16 @@ async def time_fetch(ctx):
 @bot.event
 async def on_message(message):
     # メッセージが/time_fetchだった場合:
+    target_username = "kwat0_0d065"#ユーザー名
 
     if message.author.bot:
         return
-    await bot.process_commands(message)
-    await message.channel.send(message.content)
+    if message.content:
+        target_user = discord.utils.get(message.guild.members, name=target_username)
+        if target_user:
+
+            await bot.process_commands(message)
+            await message.channel.send(f"{target_user.mention}{target_user.mention}\n{target_user.mention}{target_user.mention}")
+
 
 bot.run(config_data["discord_token"])
