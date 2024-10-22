6b9b4a7b4ece1e94c94abdb5acedaf16ae7b82e3
kWat0M <science.and.technology.77@gmail.com>
2024-10-23 00:42:07 +0900

    修正

--- APP/test_ephemeral_msg.py
+++ APP/test_ephemeral_msg.py
@@ -1,15 +1,16 @@
 import discord
+from discord.ext import commands 
 
-async def test_ephe(message):
-    if message.author.bot:
+async def test_ephe(ctx, interaction_response : InteractionResponse):
+    if ctx.author.bot:
         return
 
     #embedの表示
-    author = message.author
+    author = ctx.author
     embed = discord.Embed(title="踊れスネ夫!", description="Here you are", color=0xff0000)
     embed.set_image(url='https://images-ext-1.discordapp.net/external/2EYEX2fHQoHNLdgfjYjMqAwY5CnADNx01IiIyBr49k4/https/media1.tenor.com/m/4poLNzFGYTwAAAAC/suneo-dance-dance.gif?width=655&height=655')
     embed.set_author(name=author)
-    await message.channel.send(embed = embed)
+    await ctx.channel.send(embed = embed)
 
     #ephemeral messageの送信
     interaction_response = discord.InteractionResponse()
