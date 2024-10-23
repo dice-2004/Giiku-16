4ce72f588a020c291d7038306b1c140aaa1fc42b
NAoMi <nogamenolife9589@gmail.com>
2024-10-23 18:41:48 +0900

    delete

--- APP/app.py
+++ APP/app.py
@@ -1,46 +1,11 @@
-from modules import fetch_class, notion_class
-# # from discord import Intents,Bot
-# import time
-# import discord
-# from discord import app_commands
-# from discord.ext import commands
-
-# #################################################################################################################################################################
-fetch = fetch_class.fetcher()
-config_data = fetch.fetch()
-# # notion = notion_class.Notion(config_data["notion_token"])
-
-# # # print(config_data)
-# # str = notion.get_pages(config_data["database_id"])
-# # print(str)
-# #################################################################################################################################################################
-
-
-# intents =discord.Intents.default()
-# client = discord.Client(intents=intents)
-# tree = app_commands.CommandTree(client)
-
-
-# bot=commands.Bot(
-#         command_prefix="!",
-#         intents=intents,
-#         activity=discord.Game("いまうごいたわ"),
-# )
-
-# async def load_extensions():
-#     await bot.load_extension("cogs.botton")  # Cogファイルのロード
-
-# @bot.event
-# async def on_ready():
-#     await load_extensions()
-#     print("Bot is ready")
-
-
-# client.run(config_data["discord_token"])
-
-
+from modules import fetch_class
 import discord
 from discord.ext import commands
+
+fetch = fetch_class.fetcher()
+config_data = fetch.fetch()
+
+
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
