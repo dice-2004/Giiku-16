b8ef24cd9a3c724aa7159c29676a52425f84b57d
kWat0M <science.and.technology.77@gmail.com>
2024-10-26 23:11:16 +0900

    自動通知の完成とファイル分け

---
+++ APP/cogs/auto_announce.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+
+class AutoAnnounce():
+
+    async def auto_disp_button(bot : commands.Bot, CHANNEL_ID : int):
+        print("1")
+        ch = bot.get_channel(CHANNEL_ID)
+        if ch is None:
+            print("チャンネルが見つかりません。CHANNEL_IDが正しいか確認してください。")
+            return
+        print("2")
+        JST_time = datetime.now()
+        now = str (JST_time).split(" ")[1].split(".")[0].split(":")[0] + str (JST_time).split(" ")[1].split(".")[0].split(":")[1]
+        if(now != "2241"):
+            await ch.send(now)
+            button_embed = discord.Embed(title="今日、明日が期限の予定を表示します")
+            await ch.send(embed=button_embed)
+            button = discord.ui.Button(label="確認",style=discord.ButtonStyle.primary,custom_id="check")
+            view = discord.ui.View()
+            view.add_item(button)
+            await ch.send(view=view)
+            print("3")
+
+    async def on_button_click(inter:discord.Interaction):
+        custom_id = inter.data["custom_id"]
+        if custom_id == "check":
+            task_embed = discord.Embed(
+                title = "あなたのユーザー名",
+                description = inter.user.name + "#" + inter.user.discriminator,
+                color = 0x0000ff
+            )
+            await inter.response.send_message(embed=task_embed,ephemeral=True)
--- APP/kw.py
+++ APP/kw.py
@@ -1,7 +1,7 @@
 from modules import fetch_class
 import discord  # ver.2.3.2
 from discord.ext import commands, tasks
-from datetime import datetime, timezone
+from cogs.auto_announce import AutoAnnounce
 
 fetch = fetch_class.fetcher()
 config_data = fetch.fetch()
@@ -14,17 +14,9 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 CHANNEL_ID = 1297908587141791845
 #client = discord.Client(intents=intents)
 
-@tasks.loop(seconds=10)
+@tasks.loop(seconds=60)
 async def loop():
-    now = datetime.now(timezone.utc)
-    print("1")
-    ch = bot.get_channel(CHANNEL_ID)
-    if ch is None:
-        print("チャンネルが見つかりません。CHANNEL_IDが正しいか確認してください。")
-        return
-    print("2")
-    await ch.send(str(now))
-    print("3")
+    await AutoAnnounce.auto_disp_button(bot,CHANNEL_ID)
 
 @bot.event
 async def on_ready():
@@ -33,11 +25,18 @@ async def on_ready():
         print("loopstart")
         loop.start()
 
+@bot.event
+async def on_interaction(inter:discord.Interaction):
+    try:
+        if inter.data['component_type'] == 2:
+            await AutoAnnounce.on_button_click(inter)
+    except KeyError:
+        pass
+
 async def load_extensions():
     await bot.load_extension("cogs.botton")
     await bot.load_extension("cogs.template")
     await bot.load_extension("cogs.test_notion")
-    await bot.load_extension("cogs.test_auto")
 
 async def main():
     async with bot:
