f15eceb6a9f1ba9b1ac18b2658b30606792af255
kWat0M <science.and.technology.77@gmail.com>
2024-10-27 05:23:10 +0900

    通知時間を変数に

--- APP/app.py
+++ APP/app.py
@@ -1,23 +1,26 @@
-from modules import fetch_class
+from modules import fetch_class, notion_class
 import discord  # ver.2.3.2
 from discord.ext import commands
 
 #自動通知機能で使用
 from discord.ext import commands, tasks
-from APP.modules.auto_announce import AutoAnnounce
+from modules.auto_announce import AutoAnnounce
 
+#config.ini内のデータの取得
 fetch = fetch_class.fetcher()
 config_data = fetch.fetch()
+notion = notion_class.Notion(config_data["notion_token"])
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 CHANNEL_ID = 1297908587141791845
+TIME = "0900"
 
 #指定時間で通知するためのループ
 @tasks.loop(seconds=60)
 async def loop():
-    await AutoAnnounce.auto_disp_button(bot,CHANNEL_ID)
+    await AutoAnnounce.auto_disp_button(bot,CHANNEL_ID,TIME)
 
 @bot.event
 async def on_ready():
@@ -33,7 +36,7 @@ async def on_ready():
 async def on_interaction(inter:discord.Interaction):
     try:
         if inter.data['component_type'] == 2:
-            await AutoAnnounce.on_button_click(inter)
+            await AutoAnnounce.on_button_click(inter, config_data["database_id"], notion)
     except KeyError:
         pass
 
