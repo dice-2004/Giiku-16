9365f9314dce91bd9d8b35d8f39521d14a231377
kWat0M <science.and.technology.77@gmail.com>
2024-10-26 17:39:17 +0900

    成果なし

--- APP/cogs/test_auto.py
+++ APP/cogs/test_auto.py
@@ -1,22 +1,38 @@
 import discord
 from discord import app_commands
-from discord.ext import commands
+import discord.context_managers
+from discord.ext import commands, tasks
+from datetime import datetime, timezone
+
+CHANNEL_ID = "1299615250743693322"
 
 class TestAuto(commands.Cog):  # Cogクラスの継承を修正
     def __init__(self, bot):
         self.bot = bot
         self.tree = bot.tree  # デフォルトの CommandTree を使用
+        
 
     @commands.Cog.listener()
     async def on_ready(self):
         await self.tree.sync()  # Botが準備完了したときにコマンドを同期
+
+    @tasks.loop(seconds=10)
+    async def loop():
+        client = discord.Client()
+        print("1")
+        now = datetime.now(timezone.utc)
+        print("2")
+        ch = client.get_channel(CHANNEL_ID)
+        print("3")
+        await ch.send(now)
+        print("4")
 
     @app_commands.command(name="auto", description="auto_test")
     async def template(self, interaction: discord.Interaction):
         #interaction.user = "kWat0_0d65"
         await interaction.response.defer(thinking=True)
         #interaction.message.author = "nontenki._."
-        await interaction.followup.send("test", ephemeral=True)
+        await interaction.followup.send("loop start", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(TestAuto(bot))  # ボットにCogを追加
---
+++ APP/kw.py
@@ -0,0 +1,32 @@
+from modules import fetch_class
+import discord  # ver.2.3.2
+from discord.ext import commands, tasks
+from datetime import datetime, timezone
+from cogs.test_auto import TestAuto
+
+fetch = fetch_class.fetcher()
+config_data = fetch.fetch()
+
+
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+async def load_extensions():
+    await bot.load_extension("cogs.botton")
+    await bot.load_extension("cogs.template")
+    await bot.load_extension("cogs.test_notion")
+    await bot.load_extension("cogs.test_auto")
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(config_data["discord_token"])
+        TestAuto.loop.start()
+
+import asyncio
+asyncio.run(main())
