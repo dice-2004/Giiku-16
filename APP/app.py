0244956e7434d641b513649da915d412c85ddc8a
kWat0M <science.and.technology.77@gmail.com>
2024-10-26 23:28:17 +0900

    app.pyに自動通知機能の実装

--- APP/app.py
+++ APP/app.py
@@ -2,23 +2,45 @@ from modules import fetch_class
 import discord  # ver.2.3.2
 from discord.ext import commands
 
+#自動通知機能で使用
+from discord.ext import commands, tasks
+from cogs.auto_announce import AutoAnnounce
+
 fetch = fetch_class.fetcher()
 config_data = fetch.fetch()
-
-
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+CHANNEL_ID = 1297908587141791845
+
+#指定時間で通知するためのループ
+@tasks.loop(seconds=60)
+async def loop():
+    await AutoAnnounce.auto_disp_button(bot,CHANNEL_ID)
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+    
+    #指定時間で通知するためのループを開始
+    if not loop.is_running():
+        print("loopstart")
+        loop.start()
+
+#自動通知機能でボタンが押されたときの処理
+@bot.event
+async def on_interaction(inter:discord.Interaction):
+    try:
+        if inter.data['component_type'] == 2:
+            await AutoAnnounce.on_button_click(inter)
+    except KeyError:
+        pass
 
 async def load_extensions():
     await bot.load_extension("cogs.botton")
     await bot.load_extension("cogs.template")
     await bot.load_extension("cogs.test_notion")
-    await bot.load_extension("cogs.test_auto")
 
 async def main():
     async with bot:
