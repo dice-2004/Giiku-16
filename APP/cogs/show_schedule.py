186cd736e36c7d48dfc95a3b434b416aa3add72f
NAoMi <nogamenolife9589@gmail.com>
2024-10-27 12:46:22 +0900

    create show_schedule

--- APP/cogs/show_schedule.py
+++ APP/cogs/show_schedule.py
@@ -1,7 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from modules import fetch_class
 from modules import notion_class
 
@@ -13,7 +13,7 @@ notion_token = config_data["notion_token"]
 
 nc = notion_class.Notion(notion_token)
 
-class test_notion_a(commands.Cog):  # Cogクラスの継承を修正
+class show_schedule(commands.Cog):  # Cogクラスの継承を修正
     def __init__(self, bot):
         self.bot = bot
         self.tree = bot.tree  # デフォルトの CommandTree を使用
@@ -24,32 +24,34 @@ class test_notion_a(commands.Cog):  # Cogクラスの継承を修正
         await self.tree.sync()  # Botが準備完了したときにコマンドを同期
         print("コマンド同期完了")
 
-    # @app_commands.command(name="show_data", description="表示したい個数入力可能")
-    # async def show_data(self, interaction: discord.Interaction, loop_cnt: int = 1):
-    #     print("/show_data")
-    #     data = nc.get_notion_data(database_id)
-
-    #     for i in range(loop_cnt):
-    #         await interaction.channel.send(data[i]["title"])
-
-    @app_commands.command(name="show_embed", description="埋め込みメッセージを表示")
-    async def show_embed(self, interaction: discord.Interaction, loop_cnt: int = 1):
+    @app_commands.command(name="show_schedule", description="引数で日数指定")
+    async def show_schedule(self, interaction: discord.Interaction, UntilDaysLaterIs: int = 1):
         data = nc.get_notion_data(database_id)
         sorted_data = sorted(data, key=lambda x: datetime.strptime(x["timeschedul"], "%Y/%m/%d"), reverse=False)
         filter_data = list(filter(lambda x: x["dead_line_exceed"] == False, sorted_data))
+        
+        # for i in filter_data:
+        #     print(i["title"])
 
         # Embedの作成
-        embed = discord.Embed(title=f'直近 {max(loop_cnt, len(filter_data))} 個の予定を表示します。', description="", color=discord.Color.blue())
+        embed = discord.Embed(title=f'直近 {UntilDaysLaterIs} 日の予定を表示します。', description="", color=discord.Color.blue())
         
         # フィールドを追加
-        for i in range(max(loop_cnt, len(filter_data))-loop_cnt, max(loop_cnt, len(filter_data))):
-            embed.add_field(name=f'{filter_data[i]["timeschedul"]}　{filter_data[i]["title"]}\n', value="", inline=False)
+        for i in range(min(UntilDaysLaterIs, len(filter_data))):
+            timeschedul = datetime.strptime(filter_data[i]["timeschedul"], "%Y/%m/%d").date()
+            if date.today() < timeschedul <= date.today() + timedelta(days=UntilDaysLaterIs):
+                embed.add_field(
+                    name=f'`{filter_data[i]["timeschedul"]}  {filter_data[i]["select"]}  {filter_data[i]["title"]}`',
+                    value="",
+                    inline=False
+                )
+
 
         # embed.set_thumbnail(url="https://media1.tenor.com/m/4poLNzFGYTwAAAAC/suneo-dance-dance.gif")
         embed.set_author(name=f'{interaction.user.display_name}', icon_url="https://media1.tenor.com/m/4poLNzFGYTwAAAAC/suneo-dance-dance.gif", url="https://www.notion.so/schedule-af2f277686974b18bf885570e0e54275")
 
         # Embedを送信
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot):
-    await bot.add_cog(test_notion_a(bot))  # ボットにCogを追加
+    await bot.add_cog(show_schedule(bot))  # ボットにCogを追加
